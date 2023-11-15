d4a43e5c68ac621ff00c45b0a04cff06e06c11b2
Naima Malik <noma_m@hotmail.com>
2023-11-15 08:50:25 +0100

    Get answers, add questions remaining and create progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { quiz } from './reducers/quiz';
+import React from "react";
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { quiz } from "./reducers/quiz";
 
-import { CurrentQuestion } from './components/CurrentQuestion';
+// import { CurrentQuestion } from "./components/CurrentQuestion";
+import QuestionContainer from "./components/QuestionContainer/QuestionContainer";
 
 const reducer = combineReducers({
-  quiz: quiz.reducer
+  quiz: quiz.reducer,
 });
 
 const store = configureStore({ reducer });
@@ -14,7 +15,8 @@ const store = configureStore({ reducer });
 export const App = () => {
   return (
     <Provider store={store}>
-      <CurrentQuestion />
+      {/* <CurrentQuestion /> */}
+      <QuestionContainer />
     </Provider>
   );
-}
+};
